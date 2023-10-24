66b8e7d29a66b9b45c235dbf27268311d69bec2a
Nyllaah <nylla.g@hotmail.com>
2023-10-24 15:29:00 -0300

    creating routes, layout and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
+import DoneRecipes from './pages/DoneRecipes';
+import FavoriteRecipes from './pages/FavoriteResipes';
+import Drinks from './pages/Drinks';
+import Home from './pages/Home';
+import Meals from './pages/Meals';
+import Profile from './pages/Profile';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <Routes>
+      <Route path="/" element={ <Home /> } />
+      <Route path="meals:id" element={ <Meals /> } />
+      <Route path="drinks:id" element={ <Drinks /> } />
+      <Route path="meals:id/in-progress" element={ <Meals /> } />
+      <Route path="drinks:id/in-progress" element={ <Drinks /> } />
+      <Route element={ <Layout /> }>
+        <Route path="meals" element={ <Meals /> } />
+        <Route path="drinks" element={ <Drinks /> } />
+        <Route path="profile" element={ <Profile /> } />
+        <Route path="done-recipes" element={ <DoneRecipes /> } />
+        <Route path="favorite-recipes" element={ <FavoriteRecipes /> } />
+      </Route>
+    </Routes>
   );
 }
 
---
+++ src/pages/DoneRecipes.tsx
@@ -0,0 +1,3 @@
+export default function DoneRecipes() {
+  return <h1>Done Recipes</h1>;
+}
