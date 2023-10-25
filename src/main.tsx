8140f01ee5802c2d4f0909fda1a87db3d577e589
Mandytrajano90 <mandytrajano90@gmail.com>
2023-10-24 21:44:10 -0300

    Sixth Requirement - fix lint

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// eslint-disable-next-line import/no-unresolved
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
