a5c37ebc78acdccf95469309f4529584129e7d48
Ian Anker <anker.ian@gmail.com>
2023-10-29 18:59:17 -0300

    feat: adding done recipes tests

--- src/pages/DoneRecipes.tsx
+++ src/pages/DoneRecipes.tsx
@@ -3,41 +3,6 @@ import DoneRecipesCard from '../components/DoneRecipesCard';
 import DoneRecipesFilter from '../components/DoneRecipesFilter';
 import { DoneRecipeType } from '../types';
 
-const MOCKdoneRecipesData = [{
-  id: '52772',
-  type: 'meal',
-  nationality: 'Japanese',
-  category: 'Chicken',
-  alcoholicOrNot: '',
-  name: 'Teriyaki Chicken Casserole',
-  image: 'https://www.themealdb.com/images/media/meals/wvpsxx1468256321.jpg',
-  doneDate: '30/10/2021',
-  tags: ['Meat', 'Pasta', 'Curry'],
-},
-{
-  id: '52732',
-  type: 'drink',
-  nationality: 'brasil',
-  category: 'cachaça',
-  alcoholicOrNot: 'alcoholic',
-  name: 'Caipeirinha',
-  image: 'https://www.thecocktaildb.com/images/media/drink/vyxwut1468875960.jpg',
-  doneDate: '30/10/2031',
-  tags: ['Limão', 'Cachaça', 'Gelo'],
-},
-{
-  id: '5233',
-  type: 'drink',
-  nationality: 'holanda',
-  category: 'cerveja',
-  alcoholicOrNot: 'alcoholic',
-  name: 'Brahma',
-  image: 'https://www.thecocktaildb.com/images/media/drink/xsqrup1441249130.jpg',
-  doneDate: '30/10/2231',
-  tags: ['Cristal', 'Skoll'],
-},
-];
-
 export default function DoneRecipes() {
   const [doneRecipesData, setDoneRecipesData] = useState([]);
   const [doneRecipesFilter, setDoneRecipesFilter] = useState('all');
@@ -46,7 +11,6 @@ export default function DoneRecipes() {
     const data = localStorage.getItem('doneRecipes');
     const dataJSON = data ? JSON.parse(data) : [];
     setDoneRecipesData(dataJSON);
-    // setDoneRecipesData(MOCKdoneRecipesData);
   }, []);
 
   const filterdata = (doneRecipesFilter === 'all')
