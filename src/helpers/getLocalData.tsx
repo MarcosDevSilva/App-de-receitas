91dd53b0239c4bc79924d0ea1c572f7df362b709
Nyllaah <nylla.g@hotmail.com>
2023-10-31 15:12:49 -0300

    feat: req 40 working

--- src/helpers/getLocalData.tsx
+++ src/helpers/getLocalData.tsx
@@ -1,9 +1,11 @@
 export default function getLocalData(key: string) {
   const data = localStorage.getItem(key);
+  console.log(data);
+
   if (key === 'inProgressRecipes') {
     return data ? JSON.parse(data) : {
-      meals: [],
-      drinks: [],
+      meals: {},
+      drinks: {},
     };
   }
   return data ? JSON.parse(data) : [];
